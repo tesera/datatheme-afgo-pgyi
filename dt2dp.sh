0ea1deedc0c41366a2ccfada019f0db6ebfa3d7c
Yves Richard <yves.richard@tesera.com>
2015-03-25 05:31:52 -0600

    updates docs and adds publish scripts
    also orders the resources for sql create dependency.

--- dt2dp.sh
+++ dt2dp.sh
@@ -3,15 +3,19 @@
 echo '{"name": "afgo-dataset",'
 echo '"datatheme": { "name": "afgo.pgyi", "version": "2.0.0", "repository": "https://github.com/tesera/datatheme-afgo.git"},'
 echo '"resources": ['
-    schemas=(./schemas/*.json)
-    len=${#schemas[@]}
-
-    for ((i=0; i < $len; i+=1));
-    do
-        (cat "${schemas[i]}")
-        if [ $i -lt $(($len-1)) ]; then
-            echo ','
-        fi
-    done
-
+    cat ./schemas/plot.json
+    echo ,
+    cat ./schemas/trees.json
+    echo ,
+    cat ./schemas/plot_measurement.json
+    echo ,
+    cat ./schemas/trees_measurement.json
+    echo ,
+    cat ./schemas/photo_avi.json
+    echo ,
+    cat ./schemas/treatment.json
+    echo ,
+    cat ./schemas/disturbance.json
+    echo ,
+    cat ./schemas/regeneration.json
 echo ']}'
---
+++ publi.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# deploys datatheme resources to host s3 bucket
+# usage: $ bash ./publi.sh
+
+# dependencies:
+# aws cli : http://aws.amazon.com/cli/
+# nodejs : https://nodejs.org/
+# bawlk : https://github.com/tesera/bawlk
+
+datatheme_path=s3://tesera.datathemes/afgo.pgyi
+flags="--acl public-read \
+--content-encoding utf8 \
+--cache-control no-cahe"
+
+echo "building datapackage.json"
+bash ./dt2dp.sh > ./www/datapackage.json &&
+
+mkdir ./www/awk ./www/rules
+echo "compiling bawlk rules from datapackage.json"
+node ../node_modules/bawlk/bin/bawlk.js rules -d ./www/datapackage.json -o ./www/rules
+
+echo "compiling bawlk scripts from datapackage.json"
+node ../node_modules/bawlk/bin/bawlk.js scripts -d ./www/datapackage.json -o ./www/awk
+
+echo "publishing datatheme resources to s3"
+aws s3 cp ./www/index.html $datatheme_path/index.html --content-type text/html $flags
+aws s3 cp ./www/datapackage.json $datatheme_path/datapackage.json --content-type application/json $flags
+aws s3 cp ./www/awk/ $datatheme_path/awk --recursive --content-type text/plain $flags
+aws s3 cp ./www/rules/ $datatheme_path/rules --recursive --content-type text/csv $flags
+
+echo "publishing complete"
