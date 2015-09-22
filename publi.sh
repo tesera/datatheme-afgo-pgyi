134b860dafb5b88fc14f712b143e86f3c9e8ecda
Yves Richard <yves.richard@tesera.com>
2015-09-22 13:49:10 -0600

    Update publi.sh to publish to tesera.data.dataprofiles

--- publi.sh
+++ publi.sh
@@ -11,11 +11,10 @@
 # nodejs : https://nodejs.org/
 # bawlk : https://github.com/tesera/bawlk
 
-datatheme_root=s3://tesera.dataprofiles
+datatheme_root=s3://tesera.data.dataprofiles
 datatheme_path="$datatheme_root/$2"
 
-flags="--acl public-read \
---cache-control no-cahe"
+cp_flags="--acl public-read --cache-control no-cahe"
 
 echo "building datapackage.json"
 bash ./dt2dp.sh > ./www/datapackage.json &&
@@ -29,11 +28,11 @@ bawlk scripts -d ./www/datapackage.json -o ./www/awk
 
 if [ "$1" == "put" ]; then
     echo "publishing datatheme resources to s3"
-    aws s3 cp ./www/partials/ $datatheme_path/partials --recursive --content-type text/html $flags
-    aws s3 cp ./www/index.html $datatheme_path/index.html --content-type text/html $flags
-    aws s3 cp ./www/datapackage.json $datatheme_path/datapackage.json --content-type application/json $flags
-    aws s3 cp ./www/awk/ $datatheme_path/awk --recursive --content-type text/plain $flags
-    aws s3 cp ./www/rules/ $datatheme_path/rules --recursive --content-type text/plain $flags
+    aws s3 cp ./www/partials/ $datatheme_path/partials --recursive --content-type text/html $cp_flags
+    aws s3 cp ./www/index.html $datatheme_path/index.html --content-type text/html $cp_flags
+    aws s3 cp ./www/datapackage.json $datatheme_path/datapackage.json --content-type application/json $cp_flags
+    aws s3 cp ./www/awk/ $datatheme_path/awk --recursive --content-type text/plain $cp_flags
+    aws s3 cp ./www/rules/ $datatheme_path/rules --recursive --content-type text/plain $cp_flags
     echo "publishing complete"
 fi
 
