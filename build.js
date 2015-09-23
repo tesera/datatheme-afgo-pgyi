c9f9d9db719dc7c26f158bcf9a788ab51878fd3c
Yves Richard <yves.richard@tesera.com>
2015-09-23 14:45:00 -0600

    Fix issues with build loosing resource info

--- build.js
+++ build.js
@@ -8,7 +8,8 @@ fs.createReadStream('datatheme.json')
     .pipe(JSONStream.parse())
     .pipe(es.map(function (theme, callback) {
         var resources = theme.resources.map(function (r) {
-            return JSON.parse(fs.readFileSync(r.schema, 'utf8'));
+            r.schema = JSON.parse(fs.readFileSync(r.schema, 'utf8'));
+            return r;
         });
         delete theme.resources;
         var datapackage = {
