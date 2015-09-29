4f19f8aebe3dea03d55c59d85aba3bfdd8aea671
Yves Richard <yves.richard@tesera.com>
2015-09-29 13:31:52 -0600

    Add datatheme name to build script

--- build.js
+++ build.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 'use strict';
+// usage: node ./build.js <datatheme_name>
+// example: node ./build.js dev.afgo.pgyi
+
 var fs = require('fs');
 var JSONStream = require('JSONStream');
 var es = require('event-stream');
@@ -9,6 +12,7 @@ fs.createReadStream('datatheme.json')
     .pipe(es.map(function (theme, callback) {
         var resources = theme.resources.map(function (r) {
             r.schema = JSON.parse(fs.readFileSync(r.schema, 'utf8'));
+            if(process.argv[2]) r.validator = r.validator.replace('afgo.pgyi', process.argv[2]);
             return r;
         });
         delete theme.resources;
